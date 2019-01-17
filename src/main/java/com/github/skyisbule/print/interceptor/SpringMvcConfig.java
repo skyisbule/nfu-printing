8f7a7240a2f072782d4683ad1fdff6ac39f368d6
heqiutong <heqiut@qq.com>
2019-01-17 11:04:03 +0800

    增加跨域支持

--- src/main/java/com/github/skyisbule/print/interceptor/SpringMvcConfig.java
+++ src/main/java/com/github/skyisbule/print/interceptor/SpringMvcConfig.java
@@ -2,10 +2,13 @@ package com.github.skyisbule.print.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 public class SpringMvcConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
@@ -14,6 +17,11 @@ public class SpringMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 }
